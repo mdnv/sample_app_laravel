6d9ff9fb01f347057fcbf04219f9cc16df7d6728
mdnv <mdnvng@gmail.com>
2019-12-13 15:37:13 +0700

    upgrade

--- app/Http/Controllers/api/UsersController.php
+++ app/Http/Controllers/api/UsersController.php
@@ -88,3 +88,49 @@ class UsersController extends Controller
     }
 }
 #php artisan make:controller UserController --resource --model=User
+
+    // app/Http/Controllers/BookController.php
+
+    // add these at the top of the file
+    use App\Book;
+    use App\Http\Resources\BookResource;
+
+    public function index()
+    {
+      return BookResource::collection(Book::with('ratings')->paginate(25));
+    }
+
+    public function store(Request $request)
+    {
+      $book = Book::create([
+        'user_id' => $request->user()->id,
+        'title' => $request->title,
+        'description' => $request->description,
+      ]);
+
+      return new BookResource($book);
+    }
+
+    public function show(Book $book)
+    {
+      return new BookResource($book);
+    }
+
+    public function update(Request $request, Book $book)
+    {
+      // check if currently authenticated user is the owner of the book
+      if ($request->user()->id !== $book->user_id) {
+        return response()->json(['error' => 'You can only edit your own books.'], 403);
+      }
+
+      $book->update($request->only(['title', 'description']));
+
+      return new BookResource($book);
+    }
+
+    public function destroy(Book $book)
+    {
+      $book->delete();
+
+      return response()->json(null, 204);
+    }
